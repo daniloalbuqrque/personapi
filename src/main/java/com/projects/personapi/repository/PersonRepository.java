1bb772decd75c780aaf588f82058eaaffafcb497
Danilo Albuquerque <85083396+daniloalbuqrque@users.noreply.github.com>
2021-07-27 16:40:41 -0300

    Deleting outdated src directory

--- src/main/java/com/projects/personapi/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.projects.personapi.repository;
-
-import com.projects.personapi.entities.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
