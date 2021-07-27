cf4a6bda8fd3e4ecda8d43072c981a9c4aea9147
Danilo Albuquerque <85083396+daniloalbuqrque@users.noreply.github.com>
2021-07-27 14:43:36 -0300

    New src directory

---
+++ src/main/java/com/projects/personapi/controller/PersonController.java
@@ -0,0 +1,28 @@
+package com.projects.personapi.controller;
+
+
+import com.projects.personapi.dto.MessageResponseDTO;
+import com.projects.personapi.entities.Person;
+import com.projects.personapi.repository.PersonRepository;
+import com.projects.personapi.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+
+    private PersonService personService;
+
+    @Autowired
+    public PersonController (PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody Person person) {
+            return personService.createPerson(person);
+    }
+}
